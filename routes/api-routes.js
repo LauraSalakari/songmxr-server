c4a82cca570fb9e70ae2add512e593e80e1bff16
Laura Salakari <salakari.laura@gmail.com>
2021-02-23 16:39:11 +0100

    genre search backend complete

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,7 +3,7 @@ const router  = express.Router();
 const spotifyApi = require("../config/spotify.config");
 
 // get genres
-router.get('/genres', (req, res, next) => {
+router.get('/genres', (req, res) => {
   spotifyApi.getAvailableGenreSeeds()
   .then(response => {
     //console.log(response);
@@ -13,5 +13,25 @@ router.get('/genres', (req, res, next) => {
     console.log(err);
   })
 });
+
+// get genre-based recommendations
+router.post("/genrerecommendations", (req, res) => {
+  let {seedGenres} = req.body;
+  
+  spotifyApi.getRecommendations({
+    seed_genres: seedGenres,
+    limit: 6
+  })
+  .then(data => {
+    let {tracks} = data.body;
+    let recs = tracks.map(x => {
+      return {album: x.album.name, image: x.album.images[0], artists: x.artists, href: x.href, id: x.id, name: x.name, preview: x.preview_url}
+    })
+    res.status(200).json(recs);
+  })
+  .catch(err => {
+    console.log(err);
+  })
+})
 
 module.exports = router;
